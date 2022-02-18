1aeaaa3340e36dc99342ad8721e3f7450f77adc6
Mark Moretto <otteromkram@gmail.com>
2022-02-17 20:57:46 -0600

    Update extended_gcd.go

--- src/math/extended_gcd.go
+++ src/math/extended_gcd.go
@@ -13,61 +13,50 @@ import (
 	"fmt"
 )
 
-type ExtendedGCD interface {
-	Run(a, b int32)
-	GetGCD() int32
-	GetQuotients() []int32
-	GetCoefficients() []int32
-	Example()
-	ViewSummary()
-}
-
+// Worker struct for attributes that will be used in the algorithm.
+// An instance of this will be returned when the main EGCD function is executed.
 type worker struct {
-	prev_r   int32
-	prev_s   int32
-	prev_t   int32
-	r        int32
-	s        int32
-	t        int32
+	prev_r int32
+	prev_s int32
+	prev_t int32
+	r int32
+	s int32
+	t int32
 	quotient int32
-	results  []int32
 }
 
-func NewWorker() *worker {
-	return &worker{
-		prev_s: 1,
-		s:      0,
-		prev_t: 0,
-		t:      1,
-	}
-}
-
+// Retrieve GCD.
 func (w worker) GetGCD() int32 {
 	return w.prev_r
 }
 
+// Retrieve quotients
 func (w worker) GetQuotients() []int32 {
 	return []int32{w.t, w.s}
 }
 
+// Retrieve coefficients
 func (w worker) GetCoefficients() []int32 {
 	return []int32{w.prev_s, w.prev_t}
 }
 
+// View summary of results.
 func (w worker) ViewSummary() {
-	fmt.Printf("Bezout coefficients: %d, %d\n", w.prev_s, w.prev_t)
+	fmt.Printf("Bézout coefficients: %d, %d\n", w.prev_s, w.prev_t)
 	fmt.Printf("gcd: %d\n", w.prev_r)
 	fmt.Printf("quotients by gcd: %d, %d\n", w.t, w.s)
 }
 
-func Run(a, b int32) {
+// Driver function. This returns the worker instance, which can
+// be used to retrieve various results of the algorithm, as noted above.
+func EGCD(a, b int32) *worker {
 	w := &worker{
 		prev_r: a,
-		r:      b,
+		r: b,
 		prev_s: 1,
-		s:      0,
+		s: 0,
 		prev_t: 0,
-		t:      1,
+		t: 1,		
 	}
 
 	for w.r != 0 {
@@ -76,14 +65,14 @@ func Run(a, b int32) {
 		w.prev_s, w.s = w.s, w.prev_s-(w.quotient*w.s)
 		w.prev_t, w.t = w.t, w.prev_t-(w.quotient*w.t)
 	}
-	w.ViewSummary()
+	// w.ViewSummary()
+	return w
 }
 
 func Example() {
 	var A, B int32 = 240, 46
-	var egcd ExtendedGCD
-	Run(A, B)
-	egcd.ViewSummary()
+	res := EGCD(A, B)
+	res.ViewSummary()
 }
 
 func main() {
