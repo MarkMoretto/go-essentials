7e85b1a3138f914440dd430de03bef56d800141b
Mark Moretto <otteromkram@gmail.com>
2022-04-21 08:49:47 -0500

    Update gcd_lcm.go
    Added Binary and Euclidean GCD.

--- src/math/gcd_lcm.go
+++ src/math/gcd_lcm.go
@@ -46,3 +46,33 @@ func GCD(x, y int, other ...int) (result int) {
 
 	return
 }
+
+// Euclidean GCD
+func EuclideanGCD(a, b uint32) uint32 {
+	if a%b == 0 {
+		return b
+	}
+	return EuclideanGCD(b, a%b)
+}
+
+// Binary GCD
+// Note: BinaryGCD(x, y, 1) ==> GCD(x, y)
+func BinaryGCD(a, b, res uint32) uint32 {
+	switch {
+	case a == b:
+		return res * a
+	case a%2 == 0:
+		switch b%2 == 0 {
+		case true:
+			return BinaryGCD(a/2, b/2, 2*res)
+		default:
+			return BinaryGCD(a/2, b, res)
+		}
+	case b%2 == 0:
+		return BinaryGCD(a, b/2, res)
+	case a > b:
+		return BinaryGCD(a-b, b, res)
+	default:
+		return BinaryGCD(a, b-a, res)
+	}
+}
