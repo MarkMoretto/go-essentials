1c3831235aa784abdb8b3f3ab6bfc4fa30b08d61
Mark Moretto <25012707+MarkMoretto@users.noreply.github.com>
2022-09-09 13:53:14 -0500

    Create by_test.go

---
+++ src/wrapper/string/concat/by_test.go
@@ -0,0 +1,17 @@
+package concat
+
+import "testing"
+
+// Testing example.
+func ExampleConcatByHyphen() {
+    ConcatByHyphen := ConcatBy("-")
+    fmt.Println(ConcatByHyphen("followed", "by"))
+    fmt.Println(ConcatByHyphen("followed", ""))
+    fmt.Println(ConcatByHyphen("", "by"))
+    fmt.Println(ConcatByHyphen("", ""))
+    // Output:
+    // followed-by
+    // followed-
+    // -by
+    // -
+}
